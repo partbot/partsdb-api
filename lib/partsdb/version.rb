0a840db9524768dc4b39e386bb2713be3692ef66
Liam Wallace <liam.wallace@swd.com.au>
2022-03-01 15:34:42 +1000

    init

---
+++ lib/partsdb/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Partsdb
+  VERSION = "0.1.0"
+end
---
+++ spec/partsdb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Partsdb do
+  it "has a version number" do
+    expect(Partsdb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
